8b3d0b5b728daeddfb0a39cc0e608bfcba2b9bb6
LUIS CESAR EUGENIO AMORIM <LUIS6190@DIGITALCOLLEGE.LOCAL>
2024-07-05 15:50:53 -0300

    criando um evento que troca a cor de uma div

---
+++ Tela_magica/trocaCor.js
@@ -0,0 +1,8 @@
+let vermelho = document.querySelector('.vermelho')
+let verde = document.querySelector('.verde')
+let azul = document.querySelector('.azul')
+let tela = document.querySelector('#tela')
+
+function trocarCor(cor) {
+   tela.style.background = cor
+}
